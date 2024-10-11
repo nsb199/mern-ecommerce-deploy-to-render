687246ef1801311a91e3b3f97f5175d6e828a32f
nsb199 <singhneeraj199199@gmail.com>
2024-10-11 20:45:01 +0530

    fixed env mistake

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD-NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
